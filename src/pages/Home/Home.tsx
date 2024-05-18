09757fbcb75abb90a0f9bddb9256c44b47bb6f4b
cipriandraghici <cipriandraghici93@gmail.com>
2024-05-18 22:03:42 +0300

    remove gauge and display tps as a card

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,23 @@
 import { TrendChart } from '../../components/TrendChart/TrendChart.tsx';
-import { GaugeChart } from '../../components/GaugeChart/GaugeChart.tsx';
+import { useGetLatestTps } from './hooks/useGetLatestTps.ts';
 
 export const Home = () => {
+  const { tps } = useGetLatestTps();
+  console.log('tps', tps);
+
   return (
     <div className="w-full p-1 mt-10 flex justify-center items-center my-auto flex-col gap-64">
-      <div className="flex justify-center flex-col ">
-        <GaugeChart />
+      <div className="max-w-sm w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
+        <div className="flex justify-between">
+          <div>
+            <h5 className="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">
+              TPS {tps > 0 ? tps.toFixed(2) : tps}
+            </h5>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+              in the last 30 seconds
+            </p>
+          </div>
+        </div>
       </div>
       <div className="w-full flex justify-center mx-auto text-xs">
         <TrendChart />
