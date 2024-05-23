c6ee2904b4192b57d59d6763056863e39271c09d
Dan Popa <popa.ioan.dan@gmail.com>
2024-05-23 22:59:54 +0300

    Small fix

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,16 +2,22 @@ import { ReactNode } from 'react';
 
 export const Hero = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="relative flex items-center justify-center h-screen mb-12">
-      <div className="relative flex w-full flex-col items-center md:mt-36 mt-[75rem] bg-contain">
-        <div className="md:3/4 lg:2/4 z-[1] mx-auto flex w-full flex-col items-center gap-10 md:gap-16 md:mb-14 mt-10 md:mt-36">
+    <div className="flex items-center justify-center h-full mb-12">
+      <div className="relative flex w-full flex-col items-center overflow-hidden">
+        <div className="z-[1] mx-auto flex w-full flex-col items-center gap-10 md:gap-16">
           {children}
         </div>
+
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute z-0 top-[-10rem] w-auto min-w-full min-h-full max-w-none"
+        >
+          <source src="https://cdn.multiversx.com/bg.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
-      <video autoPlay loop muted className="absolute z-0 w-auto min-w-full min-h-full max-w-none">
-        <source src="https://cdn.multiversx.com/bg.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
     </div>
   );
 };
