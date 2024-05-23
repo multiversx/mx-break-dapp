22a4fad000b646e2c384d8347334c3a711c27043
cipriandraghici <cipriandraghici93@gmail.com>
2024-05-23 15:07:00 +0300

    update explorer link

--- src/config/config.sovereign.ts
+++ src/config/config.sovereign.ts
@@ -3,4 +3,4 @@ export * from './sharedConfig';
 export const API_URL = 'https://mx-api-sov-ovh.elrond.ro';
 export const CHAIN_ID = 'S';
 export const extrasApi = 'https://extras-api-sov-ovh.elrond.ro';
-export const explorerAddress = 'https://sov-ovh.internal-explorer.multiversx.com';
+export const explorerAddress = 'https://explorer.voyager1.dev';
