c9b36b692cc72243d7dbaf9f2aef54c41762ffc3
Dan Popa <popa.ioan.dan@gmail.com>
2024-05-23 16:35:45 +0300

    Small fixes

--- src/components/TrendChart/TrendChart.tsx
+++ src/components/TrendChart/TrendChart.tsx
@@ -16,7 +16,8 @@ export function TrendChart() {
 
   return (
     <div className="flex-1 h-32 relative flex flex-col items-center">
-      <div className="float-left mr-auto ml-10 text-neutral-400 text-sm">Last hour</div>
+      <div className="absolute h-full w-32 md:w-60 left-0 chart-gradient z-10" />
+      <div className="float-left mr-auto ml-10 text-neutral-400 text-sm z-20">Last 10 minutes</div>
       <ResponsiveContainer>
         <ComposedChart data={chartData}>
           <defs>
