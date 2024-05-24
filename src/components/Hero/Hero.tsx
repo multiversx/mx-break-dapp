056888feae9ee5bb3fa4d6e6fa34e85d8abfe985
cipriandraghici <cipriandraghici93@gmail.com>
2024-05-24 13:26:45 +0300

    fix bg video

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,23 +1,15 @@
-import { ReactNode } from 'react';
-
-export const Hero = ({ children }: { children: ReactNode }) => {
+export const Hero = () => {
   return (
-    <div className="flex items-center justify-center h-full mb-12">
-      <div className="relative flex w-full flex-col items-center overflow-hidden">
-        <div className="z-[1] mx-auto flex w-full flex-col items-center gap-10 md:gap-16">
-          {children}
-        </div>
-
-        <video
-          autoPlay
-          loop
-          muted
-          className="absolute z-0 top-[-10rem] w-auto min-w-full min-h-full max-w-none"
-        >
-          <source src="https://cdn.multiversx.com/bg.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
+    <div className="absolute flex w-full flex-col items-center overflow-hidden">
+      <video
+        autoPlay
+        loop
+        muted
+        className="z-0 top-[-10rem] w-auto min-w-full min-h-full max-w-none"
+      >
+        <source src="https://cdn.multiversx.com/bg.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,35 +7,44 @@ import { SpamSection } from './components/SpamSection';
 
 export const Home = () => {
   return (
-    <Hero>
-      <div className="font-roobert-medium hero-gradient text-transparent bg-clip-text text-center text-3xl md:leading-tight md:text-6xl tracking-tight mt-12 md:mt-4">
-        Move fast and break chains <span className="hidden sm:block" />
-        with Sovereign Speed
+    <>
+      <div className="relative mt-[-8rem]">
+        <Hero />
       </div>
+      <div className="flex items-center justify-center h-full mt-32 mb-12">
+        <div className="flex w-full flex-col items-center overflow-hidden">
+          <div className="z-[1] mx-auto flex w-full flex-col items-center gap-10 md:gap-16">
+            <div className="font-roobert-medium hero-gradient text-transparent bg-clip-text text-center text-3xl md:leading-tight md:text-6xl tracking-tight mt-12 md:mt-4">
+              Move fast and break chains <span className="hidden sm:block" />
+              with Sovereign Speed
+            </div>
 
-      <div className="flex gap-3 text-center p-6 md:p-6 relative">
-        <div className="relative">
-          <TspGauge />
+            <div className="flex gap-3 text-center p-6 md:p-6 relative">
+              <div className="relative">
+                <TspGauge />
+              </div>
+            </div>
+
+            <div className="w-full relative md:flex md:flex-row md:justify-end md:gap-4 mt-0 md:mt-32">
+              <TrendChart />
+              <div className="flex-1 flex flex-col md:flex-row gap-2 md:gap-5 p-4 md:p-0">
+                <TotalTransactions />
+                <MaxTps />
+              </div>
+            </div>
+
+            <div className="text-white text-center text-4xl md:text-6xl font-roobert-medium mt-10">
+              Will it break? Can you break it?
+            </div>
+
+            <div className="w-full p-4 flex justify-center relative flex-col items-center ">
+              <div className="p-4 rounded-2xl bg-neutral-900 w-full md:max-w-[80%]">
+                <SpamSection />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="w-full relative md:flex md:flex-row md:justify-end md:gap-4 mt-0 md:mt-32">
-        <TrendChart />
-        <div className="flex-1 flex flex-col md:flex-row gap-2 md:gap-5 p-4 md:p-0">
-          <TotalTransactions />
-          <MaxTps />
-        </div>
-      </div>
-
-      <div className="text-white text-center text-4xl md:text-6xl font-roobert-medium mt-10">
-        Will it break? Can you break it?
-      </div>
-
-      <div className="w-full p-4 flex justify-center relative flex-col items-center ">
-        <div className="p-4 rounded-2xl bg-neutral-900 w-full md:max-w-[80%]">
-          <SpamSection />
-        </div>
-      </div>
-    </Hero>
+    </>
   );
 };
