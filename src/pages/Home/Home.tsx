f07a1d6902cce98f8f4f1a4b8b3df2e762ca5a7b
cipriandraghici <cipriandraghici93@gmail.com>
2024-05-27 17:10:59 +0300

    tables gap

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -45,7 +45,7 @@ export const Home = () => {
               </div>
             </div>
 
-            <div className="w-full p-4 flex justify-center relative flex-col md:flex-row md:max-w-[80%]">
+            <div className="w-full p-4 flex justify-center relative flex-col md:flex-row md:max-w-[80%] gap-6">
               <div className="p-4 rounded-2xl w-full md:max-w-[30%]">
                 <Top10 />
               </div>
