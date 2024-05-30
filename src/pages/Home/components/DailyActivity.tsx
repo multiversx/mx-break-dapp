6901063774fe5a6e9ed9b81ed3ecc6d4af916ed3
cipriandraghici <cipriandraghici93@gmail.com>
2024-05-30 14:57:03 +0300

    fix daily activity table header

--- src/pages/Home/components/DailyActivity.tsx
+++ src/pages/Home/components/DailyActivity.tsx
@@ -26,13 +26,13 @@ export const DailyActivity = () => {
           <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-neutral-900 dark:text-gray-400">
               <tr>
-                <th scope="col" className="px-4 md:px-6 py-3">
+                <th scope="col" className="px-4 md:px-6 py-3 text-left">
                   Date
                 </th>
-                <th scope="col" className="px-4 md:px-6 py-3">
+                <th scope="col" className="px-4 md:px-6 py-3 text-left">
                   TXS / Day
                 </th>
-                <th scope="col" className="px-4 md:px-6 py-3">
+                <th scope="col" className="px-4 md:px-6 py-3 text-center">
                   MAX TPS
                 </th>
                 {!isMobileDevice && (
